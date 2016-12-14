9e39bc1dc328baa0a11c7033657073094add7e51
Royce Mou <Royce Mou>
2016-12-14 01:54:46 -0800

    Changed policy

--- three_card_poker_env.py
+++ three_card_poker_env.py
@@ -54,11 +54,11 @@ class ThreeCardPokerEnv(Env):
         done : a boolean, indicating whether the episode has ended
         info : a dictionary containing other diagnostic information from the previous action
         """
-        print(action)
-        # the player folds if a is 0 and continues if a is 1
+
 
         next_observation = np.copy(self._state) # the game is over after one action, so the next state is arbitrary
 
+        # the player folds if a is 0 and continues if a is 1
         if action == 0:
             return Step(observation=next_observation, reward=-1, done=True)
 
