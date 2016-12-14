77a5d87ab36bb159e387a60c5f9ddab5d2577dc6
Royce Mou <Royce Mou>
2016-12-14 01:05:51 -0800

    Changed policy

--- three_card_poker.py
+++ three_card_poker.py
@@ -2,14 +2,14 @@ from rllab.algos.vpg import VPG
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from three_card_poker_env import ThreeCardPokerEnv
 from rllab.envs.normalized_env import normalize
-from rllab.policies.deterministic_mlp_policy import DeterministicMLPPolicy
+from rllab.policies.gaussian_gru_policy import GaussianGRUPolicy
 
 env = normalize(ThreeCardPokerEnv())
-policy = DeterministicMLPPolicy(
+policy = GaussianGRUPolicy(
     env_spec=env.spec,
 )
 baseline = LinearFeatureBaseline(env_spec=env.spec)
-algo = VPG(
+algo = ERWR(
     env=env,
     policy=policy,
     baseline=baseline,
