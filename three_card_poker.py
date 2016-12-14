d17d5c38853ea3d866071914dcc2562c1719203e
Royce Mou <Royce Mou>
2016-12-14 00:55:22 -0800

    Changed policy

--- three_card_poker.py
+++ three_card_poker.py
@@ -2,7 +2,7 @@ from rllab.algos.trpo import TRPO
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from three_card_poker_env import ThreeCardPokerEnv
 from rllab.envs.normalized_env import normalize
-from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
+from rllab.policies.uniform_control_policy import UniformControlPolicy
 
 env = normalize(ThreeCardPokerEnv())
 policy = UniformControlPolicy(
